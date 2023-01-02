5b8328892405ffeaed7010c367db4fbb358636d8
vijay mallavalli <vijaysaradhim@brightcomgroup.com>
2023-01-01 21:57:57 -0800

    hi

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -5,5 +5,5 @@ if number > 0:
     print("{} is positive" .format(number))
 elif number ==0:
     print("{} is zero" .format(number))
-else:
+else number < 0:
     print("{} is negaive" .format(number))
