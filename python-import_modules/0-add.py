3eb71ccb7ed03a658e5789a0a35ad266bc0bb796
vijay mallavalli <vijaysaradhim@brightcomgroup.com>
2023-01-03 20:23:00 -0800

    tommy

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-if __name__=="__main__":
-    from add_0 import add
-    a = 1
+from add_0 import add
+if __name__ == "__main__":
+    a = 10
     b = 2
-print("{;d} + {:d} = {:d}". format(a, b, add(a, b))
-
+print("{:d} + {:d} = {:d}". format(a, b, add(a, b)))
