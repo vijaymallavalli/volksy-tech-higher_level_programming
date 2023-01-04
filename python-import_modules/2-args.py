962a551e34d3e0e5d0cffbdcdc8ed06978675a0f
vijay mallavalli <vijaysaradhim@brightcomgroup.com>
2023-01-04 02:00:14 -0800

    h

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-if__name__ == "__main__":
+if __name__ == "__main__":
     import sys
-    length=len(sys.argv)
+    length = len(sys.argv)
     if length <= 1:
         print("0 arguments.")
     elif length == 2:
