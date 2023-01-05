1f561e30f84bd995e70d8f5130986e3928e8d6de
vijay mallavalli <vijaysaradhim@brightcomgroup.com>
2023-01-04 21:04:10 -0800

    dd

---
+++ python-import_modules/102-magic_calculation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    from magic_calculation_102 import add, sub
+    if (a < b):
+        c = add(a, b)
+        for z in range(4, 6):
+            c = add(c, z)
+        return(c)
+    return(sub(a, b))
