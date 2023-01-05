b6ee190e862b36313c7139ed2dbf612821c4d1dc
vijay mallavalli <vijaysaradhim@brightcomgroup.com>
2023-01-05 07:27:32 -0800

    dd

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    if i in(my_list. reversed()):
+    if i in(my_list.reversed()):
         for i in my_list:
             print('{:d}'.format(i))
