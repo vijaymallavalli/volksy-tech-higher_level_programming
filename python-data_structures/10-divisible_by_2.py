3b677dd63dbdc801741167a187c76c508d6ec2d4
vijay mallavalli <vijaysaradhim@brightcomgroup.com>
2023-01-05 22:41:54 -0800

    d

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     lst = []
-    for i in range(len(my_list)):
-        if % 2 == 0:
+    for i in my_list:
+        if i % 2 == 0:
             lst.append(True)
         else:
             lst.append(False)
