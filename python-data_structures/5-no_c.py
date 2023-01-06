51cbb23d4f51f56d8c4dc16f8f359e017bba2a7a
vijay mallavalli <vijaysaradhim@brightcomgroup.com>
2023-01-05 21:23:07 -0800

    a

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    lst = []
     for i in my_string:
-        lst=[]
         if i != 'c' and i != 'C':
             lst.append(i)
     return "".join(lst)
