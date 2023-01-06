0e42ab5f5aba65401fcf02eb9ba2efdca47b61d3
vijay mallavalli <vijaysaradhim@brightcomgroup.com>
2023-01-06 01:18:45 -0800

    dd

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -3,7 +3,7 @@ def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
     big = my_list[0]
-    for i in range (len(my_list)):
-        if my_list[i] > big:
-            big = my_list[i]
-        return big
+    for i in my_list:
+        if i > big:
+            big = i
+    return big
