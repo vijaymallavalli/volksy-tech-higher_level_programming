3a00d65f4981e4f42bd516cf6ba1614c8a134e7c
vijay mallavalli <vijaysaradhim@brightcomgroup.com>
2023-01-07 22:59:54 -0800

    d

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-         [print("{}: {}".format(v, a_dictionary[v])) for v in sorted(a_dictionary)]
+    [print("{}: {}".format(v, a_dictionary[v])) for v in sorted(a_dictionary)]
