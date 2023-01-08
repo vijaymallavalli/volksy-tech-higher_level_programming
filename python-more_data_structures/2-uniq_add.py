736cc9dbcdb705509a42e13e59bd5f43be3790ee
vijay mallavalli <vijaysaradhim@brightcomgroup.com>
2023-01-07 22:01:17 -0800

    d

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     new_list = list(set(my_list))
-    count = 0
-    for i in my_list:
-        count += i
-    return count
+    return sum(new_list)
