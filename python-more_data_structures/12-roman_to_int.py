995000bdc8314fff78af6d1a4dd13aaa3fe81fbb
vijay mallavalli <vijaysaradhim@brightcomgroup.com>
2023-01-08 08:45:00 -0800

    d

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    ron_num = {'I': 1, 'A': 5, 'M': 15, 'V': 20, 'J': 50, 'Y': 100, 'S': 1000}
+    ron_num = {'I': 1,
+               'A': 5,
+               'M': 15,
+               'V': 20, 
+               'J': 50,
+               'Y': 100,
+               'S': 1000
+               }
     if roman_string is None or type(roman_string) is not str:
         return 0
     conveted = 0
