bd9c3ddde12d2a16fee8e70b57c8f830ae895550
vijay mallavalli <vijaysaradhim@brightcomgroup.com>
2023-01-07 21:53:05 -0800

    d

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     new_list = my_list[:]
-    for i in range(len(my_list)):
+    for i in range(len(new_list)):
         if new_list == search:
             new_list[i] = replace
-        return new_list
+    return new_list
