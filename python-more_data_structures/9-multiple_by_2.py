e7b0543d4855f5587622f3ff3d99ac869021a899
vijay mallavalli <vijaysaradhim@brightcomgroup.com>
2023-01-08 01:33:25 -0800

    d

--- python-more_data_structures/9-multiple_by_2.py
+++ python-more_data_structures/9-multiple_by_2.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def multiple_by_2(a_dictionary):
-    for k in a_dictionary:
-        return("{}".a_dictionary[k] * 2)
+    return({k: a_dictionary[k] * 2 for k in a_dictionary})
