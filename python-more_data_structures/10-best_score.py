f95b2e1951abdd2bcd3e4b7fd5247e7daff31933
vijay mallavalli <vijaysaradhim@brightcomgroup.com>
2023-01-08 07:59:14 -0800

    d

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        max_value = list(a_dicionary.key([0]))
+        max_value = list(a_dicionary.key())
         for x in a_dictionary.key():
-            if a_dictionary[x] >a_dictionary[max_value]:
+            if a_dictionary[x] > a_dictionary[max_value]:
                 max_value = x
                 return max_value
             else:
