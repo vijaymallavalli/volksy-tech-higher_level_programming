24601cbb30977eeb2399e5186a8db644cf39de89
vijay mallavalli <vijaysaradhim@brightcomgroup.com>
2023-01-09 02:12:13 -0800

    dd

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -5,6 +5,6 @@ def weight_average(my_list[]):
     avg = 0
     weight = 0
     for x in my_list:
-        avg += x[0] * [1]
+        avg += x[0] * x[1]
         weight += x[1]
     return avg / weight
