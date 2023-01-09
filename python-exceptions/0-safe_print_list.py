80db7236efe022b8af3d7d06441ee3dc0ad6c6e8
vijay mallavalli <vijaysaradhim@brightcomgroup.com>
2023-01-09 05:02:58 -0800

    dd

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    y = 0
+    for i in range(0, x):
+        print("{:d}".format(my_list[y]), end"")
+        y +=1
+        except:
+            pass
+        print('')
+        return y
