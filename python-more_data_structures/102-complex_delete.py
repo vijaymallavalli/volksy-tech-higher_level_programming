e5fe23f8111fe056b24378ff56d7a7f598b42a87
vijay mallavalli <vijaysaradhim@brightcomgroup.com>
2023-01-09 02:23:39 -0800

    dd

--- python-more_data_structures/102-complex_delete.py
+++ python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    for key in lst(a_dictionary):
+    for key in list(a_dictionary):
         if value == a_dictionary[key]:
             del a_dictionary[key]
     return a_dictionary
