336359434c023f5b0e50cdfb993aeb3acac52371
vijay mallavalli <vijaysaradhim@brightcomgroup.com>
2023-01-09 10:51:19 -0800

    d

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-   """  Args:
-          my_list_1 (list): The first list.
-          my_list_2 (list): The second list.
-          list_length (int): The number of elements to divide.
-    Returns:"""
-
-    new _list=[]
+    new_list=[]
     for i in range(0, list_length):
         try:
             div = my_list_1[i] / my_list_2[i]
