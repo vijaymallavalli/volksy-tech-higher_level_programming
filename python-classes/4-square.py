9793916b7f8ad2968e82ba110870b5e02a234863
vijay mallavalli <vijaysaradhim@brightcomgroup.com>
2023-01-11 09:19:38 -0800

    d

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -18,5 +18,5 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-    def area(self):
-        return self.__size * self.__size
+def area(self):
+    return self.__size * self.__size
