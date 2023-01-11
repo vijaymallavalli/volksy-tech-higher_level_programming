3bc9c87f84042bb8ae2a53df38f7c311f9913269
vijay mallavalli <vijaysaradhim@brightcomgroup.com>
2023-01-11 04:28:32 -0800

    d

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -8,7 +8,7 @@ class Square:
         """constructor"""
         if type(size) is not int:
             raise TypeError("size must be an integer")
-        if size > 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
 
