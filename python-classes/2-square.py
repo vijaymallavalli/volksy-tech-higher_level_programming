3db480e9097c8f2f988915d25d1b283269880c9b
vijay mallavalli <vijaysaradhim@brightcomgroup.com>
2023-01-11 03:35:57 -0800

    d

---
+++ python-classes/2-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Size validation"""
+
+class Square:
+    """size validation"""
+    def __init__(self, size=0):
+        """conatructor"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
