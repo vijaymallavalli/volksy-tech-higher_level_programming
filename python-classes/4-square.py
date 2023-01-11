1b404ec21c40f6717aa66168c1e76d83cdf5ff9f
vijay mallavalli <vijaysaradhim@brightcomgroup.com>
2023-01-11 09:16:29 -0800

    d

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
-"""size of square"""
+"""size of squaree"""
 
 
 class Square:
-    """size of square"""
+    """A class that defines a square by size and can compute area"""
     def __init__(self, size=0):
         self.size = size
 
-     @property
-     def size(self):
-         return self.__size
+    @property
+    def size(self):
+        return self.__size
 
-     @size.setter
-     def size(self, size):
-         if type(size) != int:
-             raise TypeError("size must be an integer")
-         if size < 0:
-             raise ValueError("size must be >= 0")
-         self.__size = size
-
-     def area(self):
-         return self.__size * self.__size
+    @size.setter
+    def size(self, size):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    def area(self):
+        return self.__size * self.__size
