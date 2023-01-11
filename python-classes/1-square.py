784f7060df300bddec2af8c583bef23a729cb257
vijay mallavalli <vijaysaradhim@brightcomgroup.com>
2023-01-10 20:32:58 -0800

    d

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -2,8 +2,8 @@
 """square with sizes"""
 
 
-class square
+class Square
 """square with size"""
-def ___init__(self, self):
+def ___init__(self, size):
     """sixe of square"""
     self.__size = size
