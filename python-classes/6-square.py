2ab6a2ca0996a78d3ee376d6f5c8ea6bc2a32325
vijay mallavalli <vijaysaradhim@brightcomgroup.com>
2023-01-12 02:04:11 -0800

    d

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -34,7 +34,7 @@ class Square:
         """set the position"""
         if type(value) != tuple or len(value) != 2 or type(value[0]) != int\
            or type(value[1]) != int or value[0] < 0 or value[1] < 0:
-                raise TypeError("position must be a tuple of 2 positive integer")
+                raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
     def area(self):
