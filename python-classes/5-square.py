f7b319a76f34a775176f06f74b434e45140d1eb9
vijay mallavalli <vijaysaradhim@brightcomgroup.com>
2023-01-11 20:51:06 -0800

    d

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -17,7 +17,7 @@ class Square:
             raise TypeError("size must be an integer")
       if type(size) < 0:
           raise ValueError("size must be >= 0")
-      self.__size = size
+          self.__size = size
 
     def area(size):
         return self.__size * self.__size
