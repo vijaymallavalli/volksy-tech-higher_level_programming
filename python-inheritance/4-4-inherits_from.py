e883e135fbedf72871d8fc9fc0d9774ebdb0506f
vijay mallavalli <vijaysaradhim@brightcomgroup.com>
2023-01-13 01:22:23 -0800

    d

---
+++ python-inheritance/4-4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Only sub class of"""
+
+def inherits_from(obj, a_class):
+    """nstance of a class that inherited"""
+    if isinstance(obj, a_class):
+        return True
+    elif type(obj):
+        return True
+    else:
+        return False
