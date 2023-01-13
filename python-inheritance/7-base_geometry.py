b02e4aded8447c5edf4f99b05989d2fff5e9d49f
vijay mallavalli <vijaysaradhim@brightcomgroup.com>
2023-01-13 02:22:19 -0800

    d

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,15 @@
+#!/uar/bin/python3
+"""inheritance"""
+
+
+class BaseGeometry:
+    """integer validiataion"""
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """public instance method"""
+        if type(value) is != int:
+            raise TypeError("<name> must be an integer")
+        if type(value) <= 0:
+            raise ValueError("<name> must be greater than 0")
