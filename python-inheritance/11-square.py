0e27551add536d96d336431ab35bb2ed6af0537d
vijay mallavalli <vijaysaradhim@brightcomgroup.com>
2023-01-16 00:42:48 -0800

    11-square.py

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defines a Rectangle subclass Square."""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Represent a square."""
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
