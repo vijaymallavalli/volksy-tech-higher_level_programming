b3bf3f5c89c29614b7570cb60c998833e72b11aa
vijay mallavalli <vijaysaradhim@brightcomgroup.com>
2023-01-18 03:39:25 -0800

    d

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""writing a name to a file"""
+
+
+def write_file(filename="", text=""):
+    """no file exceptions required"""
+    with open(filename, "w", enconding="utf-8") as f:
+        f.write(text)
+        return len(text)
