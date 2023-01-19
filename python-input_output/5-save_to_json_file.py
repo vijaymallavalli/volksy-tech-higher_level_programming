37f08d0fb8ce597820551ebf6bb527d07cda470a
vijay mallavalli <vijaysaradhim@brightcomgroup.com>
2023-01-18 20:54:22 -0800

    d

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -4,5 +4,4 @@
 def save_to_json_file(my_obj, filename):
     """json"""
     with open(filename, "w", encoding="utf=8") as f:
-        f.write(json.dumps(my_obj))
-        return(json.dumps(my-obj))
+        return(f.write(json.dumps(my_obj))
