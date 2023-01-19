6f3ea572d6ab57807dc4aff936d8d2b7bef5e24a
vijay mallavalli <vijaysaradhim@brightcomgroup.com>
2023-01-18 20:17:53 -0800

    d

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Append to a file"""
+
+def append_write(filename="", text=""):
+    """returns a file"""
+    with open(filename, "a", encoding="utf=8") as f:
+        f.write(text)
+        return(len(text))
