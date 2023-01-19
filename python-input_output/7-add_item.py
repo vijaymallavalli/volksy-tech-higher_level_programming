8ad0b8a270f9dd4cf4f4e4ab820f1d6c652a6801
vijay mallavalli <vijaysaradhim@brightcomgroup.com>
2023-01-19 01:08:18 -0800

    d

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
-"""file added."""
-from pathlib import Path
+"""
+Script that adds all arguments to a Python list,
+then saves them to a file
+"""
 import sys
-import json
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+fname = "add_item.json"
+List = []
 
-save_to_json_file = _import_('5-save_to_json_file').save_to_json_file
-load_from_json_file = _import_('6-load_from_json_file').load_from_json_file
-
-args = []
-if Path('add_item.json').exists():
-    args = load_from_json_file('add_item.json')
+try:
+    List = load_from_json_file(fname)
+except:
+    pass
 
 for i in range(1, len(sys.argv)):
-    args.append(str(sys.argv[i]))
-
-save_to_json_file(args, 'add_item.json')
+    List.append(sys.argv[i])
+save_to_json_file(List, fname)
