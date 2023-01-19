2972d6899ebedc4915c6e00f3d411087e5e1bca2
vijay mallavalli <vijaysaradhim@brightcomgroup.com>
2023-01-18 21:19:41 -0800

    d

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -5,5 +5,5 @@ import json
 
 def load_from_json_file(filename):
     """json  loads"""
-    with open(filename, "r", encoding="utf=8") as f:
-        return(json.load(filename)
+    with open(filename, "r", encoding="utf-8") as f:
+        return(json.load(f))
