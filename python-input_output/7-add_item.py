da6629f216e2359e9a79a0290c7e104aa5a5f926
vijay mallavalli <vijaysaradhim@brightcomgroup.com>
2023-01-18 22:28:19 -0800

    d

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,4 +6,4 @@ import json
 def save_to_json_file(my-obj, filename):
     """write a file using json representation"""
     with open(filename, "w", encoding="utf-8") as f:
-        return(json.dump(my_obj, f))
+        json.dump(my_obj, f)
