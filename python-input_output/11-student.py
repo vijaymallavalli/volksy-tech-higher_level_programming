2a7d6f91c1fea5c1b6101f1e78499fa3071ee425
vijay mallavalli <vijaysaradhim@brightcomgroup.com>
2023-01-19 22:13:33 -0800

    d

---
+++ python-input_output/11-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""students modules"""
+
+
+class Student:
+    """students clas"""
+    def __init__(self, first_name, last_name, age):
+        """initilization"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        restriltiociton a dicitionary repressntation of """
+        dict = vars(self)
+        if attrs is None:
+            return dict
+
+        studentInfo = {}
+        for item in attrs:
+            if item in dict:
+                studentInfo(item) = dict[item]
+        return studentInfo
+
+    def reload_from_json(self, json):
