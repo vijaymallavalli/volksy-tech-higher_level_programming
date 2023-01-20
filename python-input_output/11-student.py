b649a9104da47ef8b017f53d62ff3c48c9f6147c
vijay mallavalli <vijaysaradhim@brightcomgroup.com>
2023-01-19 22:27:00 -0800

    d

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,28 +1,17 @@
 #!/usr/bin/python3
-"""students modules"""
+"""Defines a class Student."""
 
 
 class Student:
-    """students clas"""
-    def __init__(self, first_name, last_name, age):
-        """initilization method"""
+    """Represent a student."""
+
+    def _init_(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """
-        restriltiociton a dicitionary repressntation of students 
-        instance
-        """
-        dict = vars(self)
-        if attrs is None:
-            return dict
-
-        studentInfo = {}
-        for item in attrs:
-            if item in dict:
-                studentInfo[item] = dict[item]
-        return studentInfo
-
-    def reload_from_json(self, json):
+        """Get a dictionary representation of the Student."""
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
