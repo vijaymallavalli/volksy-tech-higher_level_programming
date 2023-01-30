32c8f51cf6573c4f5dc63dfeef6d06d8e340b514
vijay mallavalli <vijaysaradhim@brightcomgroup.com>
2023-01-30 02:12:11 -0800

    d

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,3 +1,3 @@
 -- sql
-CREATE TABLE IF NOT EXISTS second_table (int, name, score);
+CREATE TABLE IF NOT EXISTS second_table (int, name VARCHAR(256), score INT);
 INSERT INTO second_table VALUES(1, "john", 10), (2, "Alex", 3), (3, "Bob", 14), (4, "George", 8);
