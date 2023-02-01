7049740403c58c01984c5a1813032a164daccff8
vijay mallavalli <vijaysaradhim@brightcomgroup.com>
2023-02-01 03:56:11 -0800

    d

--- SQL_more_queries/1-create~-user.sql
+++ SQL_more_queries/1-create~-user.sql
@@ -1,8 +1,9 @@
--- sql
-create user if not exists user_0d_1@localhost
-identified by user_of_1_pwd;
-grant all privileges
-on *.*
-to user_0d'@'localhost
-identified by user_od_1_pwd;
-flush privileges;
+-- SQL
+CREATE USER
+    IF NOT EXISTS 'user_0d_1'@'localhost'
+    IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES
+   ON *.*
+   TO 'user_0d_1'@'localhost'
+   IDENTIFIED BY 'user_0d_1_pwd';
+FLUSH PRIVILEGES;
