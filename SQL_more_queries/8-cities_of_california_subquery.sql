aec8c3559de52975b87087a1827e727eedf6991b
vijay mallavalli <vijaysaradhim@brightcomgroup.com>
2023-02-02 03:11:46 -0800

    d

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,7 @@
--- SQL
-SELECT ID ,NAME FROM CITIES WHERE state_id IN (SELECT ID FROM states where name ='califorina') ORDER BY ID;
+-- A script that lists all the cities of California that can be f
+SELECT id, name
+FROM cities
+WHERE state_id IN (SELECT id
+      	    FROM states
+	    WHERE name = 'California')
+ORDER BY id ASC;
