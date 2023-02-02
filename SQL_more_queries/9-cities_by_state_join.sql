237e83627853e9cb0ca473370742f604743eaee2
vijay mallavalli <vijaysaradhim@brightcomgroup.com>
2023-02-02 03:21:48 -0800

    d

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,5 @@
--- SQL
-SELECT C.ID,C.NAME,S,NAME FROM cities AS C JOINT STATE_ID AS S
-ON C.STATEID = S.ID OREDER BY C.ID;
+-- A script that lists all cities contained in the database hbtn_0d_usa
+SELECT cities.id, cities.name, states.name
+FROM cities
+LEFT JOIN states ON cities.state_id=states.id
+ORDER BY cities.id;
