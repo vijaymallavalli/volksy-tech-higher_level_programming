f4bb915d00fcdb7a26143aeef796f30f1667df14
vijay mallavalli <vijaysaradhim@brightcomgroup.com>
2023-02-02 01:30:18 -0800

    d

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,3 +1,3 @@
 -- sql
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states(id INT auto_incremet not null, name VARCHAR(256),PRIMARY KEY(ID));
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(id INT auto_incremet not null, name VARCHAR(256),PRIMARY KEY(ID));
