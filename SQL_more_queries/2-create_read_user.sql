c836d32ded73f9e674035a7b3dd8016d31c7d2ae
vijay mallavalli <vijaysaradhim@brightcomgroup.com>
2023-02-01 23:07:09 -0800

    d

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
--- sql create
-create database if not exists hbtn_0d_2;
-create user if not exists user_od_2@localhost identified by 'user_0d_2_pwd';
-grant select on hbtn_0d_2.* to user_0d_2@localhost;
+--  A script that creates thE DATABASE
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
