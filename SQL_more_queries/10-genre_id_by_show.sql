63734889b047769f2dd493fd02c86907fc74f668
vijay mallavalli <vijaysaradhim@brightcomgroup.com>
2023-02-02 04:15:01 -0800

    d

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,3 +1,5 @@
 -- sql
-SELECT tv_shows.title , tv_show_genres.genre_id FROM tv_shows INNER JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
-order by tv_shows.title,tv_shows_genre_id;
+SELECT tv_shows.title,tv_show_genres.genre_id 
+FROM tv_shows INNER JOIN tv_show_genres 
+ON tv_show_genres.show_id = tv_shows.id
+ORDER BY tv_shows.title,tv_shows_genre_id;
