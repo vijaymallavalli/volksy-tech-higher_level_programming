e2baf0017b0cb429f7439f40e56480a44da8f734
vijay mallavalli <vijaysaradhim@brightcomgroup.com>
2023-02-07 23:05:17 -0800

    d

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
-"""imoorting mysqldb """
-if __name__== "__main__":
+"""
+lists all states from the database in ascending order
+using MySQLdb
+"""
+if __name__ == "__main__":
     from sys import argv
-    import  MySQLdb
-    db = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2], db=argv[3])
-    cur=db.cursor()
-    cur.execute("SELECT id, name FROM states ORDER by states.id ASC")
+    import MySQLdb
+    db = MySQLdb.connect(host="localhost", user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT id, name FROM states ORDER BY states.id ASC")
     rows = cur.fetchall()
-    for i  in rows:
-        print(i)
+    for row in rows:
+        print(row)
     cur.close()
     db.close()
