997b90e48ddc3e784ea549e0a9d0d72b6a415acb
vijay mallavalli <vijaysaradhim@brightcomgroup.com>
2023-02-08 01:25:37 -0800

    d

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
                          passwd=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute(" SELECT * FROM states WHERE name = %(name)s \
-            ORDER BY states.id ASC",{'name': argv[4]})
+            ORDER BY states.id ASC", {'name': argv[4]})
     rows = cur.fetchall()
     for row in rows:
         print(row)
