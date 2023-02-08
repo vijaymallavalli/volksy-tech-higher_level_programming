34fbc2ffcd31776828465fde96aed947eb2b5f50
vijay mallavalli <vijaysaradhim@brightcomgroup.com>
2023-02-08 02:07:44 -0800

    d

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -8,7 +8,6 @@ if __name__ == "__main__":
     cur = db.cursor()
     cur.execute("SELECT cities.name FROM cities \
                 INNER JOIN states ON cities.state_id = state.id \
-                WHERE states.name = %s \
                 ORDER BY cities.id ASC")
     rows = cur.fetchall()
     for row in rows:
