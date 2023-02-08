fd6a971192788279ab36b96c34929d3be136ecf7
vijay mallavalli <vijaysaradhim@brightcomgroup.com>
2023-02-08 04:45:46 -0800

    d

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -5,7 +5,7 @@
 import sys
 import MySQLdb
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],\
                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
     cur = db.cursor()
     cur.execute("SELECT cities.name from cities" +
