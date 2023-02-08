7041a42e442c6e7b78c2c31d1d37cd48690d4901
vijay mallavalli <vijaysaradhim@brightcomgroup.com>
2023-02-08 01:19:31 -0800

    d

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""importing sql to pythondatabase"""
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+    db = MySQLdb.connect(host="localhost", user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute(" SELECT * FROM states WHERE name = %s ORDER BY id ASC",[sys.argv[4]])
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
