61ff1b1209916a4df115b3cbf0c2cc3739eb4589
vijay mallavalli <vijaysaradhim@brightcomgroup.com>
2023-02-08 21:54:12 -0800

    d

---
+++ SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,24 @@
+-- list all genres not linked to the show Dexter
+-- USING ONLY ONE JOIN :p 
+
+SELECT name FROM tv_genres WHERE tv_genres.id not in(
+SELECT tv_show_genres.genre_id FROM tv_shows
+JOIN tv_show_genres
+ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title = 'Dexter'
+)
+ORDER BY tv_genres.name;
+
+
+-- SQL                                        
+-- SQL                                        
+                                              
+                                              
+--SELECT name                                   
+--FROM tv_genres where id not in(               
+--select tv_show_genres.genre_id                
+--from tv_shows join tv_show_genres             
+--on tv_show_genres.show_id = tv_shows.id       
+--where (title = 'Dexter'))                     
+--ORDER BY name;                                
+                                              
