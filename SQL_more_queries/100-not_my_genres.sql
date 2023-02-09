acca1b74dbf363ea1ad6b937b71218cc63041a04
vijay mallavalli <vijaysaradhim@brightcomgroup.com>
2023-02-09 02:50:51 -0800

    d

--- SQL_more_queries/100-not_my_genres.sql
+++ SQL_more_queries/100-not_my_genres.sql
@@ -1,23 +1,12 @@
 -- list all genres 
 -- USING ONLY ONE JOIN :p 
-
-SELECT name FROM tv_genres 
-WHERE tv_genres.id not in(
-SELECT tv_show_genres.genre_id 
-FROM tv_shows
-JOIN tv_show_genres
-ON tv_show_genres.show_id = tv_shows.id
-WHERE tv_shows.title = 'Dexter'
-)
+SELECT tv_genres.name
+FROM tv_genres WHERE tv_genres.names NOT IN(
+SELECT tv_genres.name
+FROM tv_genres
+LEFT OUTER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+LEFT OUTER JOIN tv_shows
+ON tv_show_genres.show_id = tv_show.id WHERE tv_shows.title = 'Dexter'
+ORDER BY tv_genres.name)
 ORDER BY tv_genres.name;
-
-
--- SQL                                        
--- SQL                                        
---SELECT name                                   
---FROM tv_genres where id not in(               
---select tv_show_genres.genre_id                
---from tv_shows join tv_show_genres             
---on tv_show_genres.show_id = tv_shows.id       
---where (title = 'Dexter'))                     
---ORDER BY name;                                                                              
