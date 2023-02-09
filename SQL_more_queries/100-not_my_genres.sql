8a35d4a6c5cecaecf497c4904c621d7c6da0e338
vijay mallavalli <vijaysaradhim@brightcomgroup.com>
2023-02-08 22:06:11 -0800

    d

--- SQL_more_queries/100-not_my_genres.sql
+++ SQL_more_queries/100-not_my_genres.sql
@@ -3,7 +3,8 @@
 
 SELECT name FROM tv_genres 
 WHERE tv_genres.id not in(
-SELECT tv_show_genres.genre_id FROM tv_shows
+SELECT tv_show_genres.genre_id 
+FROM tv_shows
 JOIN tv_show_genres
 ON tv_show_genres.show_id = tv_shows.id
 WHERE tv_shows.title = 'Dexter'
@@ -13,13 +14,10 @@ ORDER BY tv_genres.name;
 
 -- SQL                                        
 -- SQL                                        
-                                              
-                                              
 --SELECT name                                   
 --FROM tv_genres where id not in(               
 --select tv_show_genres.genre_id                
 --from tv_shows join tv_show_genres             
 --on tv_show_genres.show_id = tv_shows.id       
 --where (title = 'Dexter'))                     
---ORDER BY name;                                
-                                              
+--ORDER BY name;                                                                              
