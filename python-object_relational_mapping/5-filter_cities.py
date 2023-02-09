c412121d4fea9d9802f72e94877190d7be7b7ebf
vijay mallavalli <vijaysaradhim@brightcomgroup.com>
2023-02-08 21:13:37 -0800

    d

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
-""" cities name """
-
-
-from sys import argv
+"""
+Script the list and filter the name the states
+"""
 import MySQLdb
+import sys
+
 if __name__ == "__main__":
-    db = MySQLdb.connect(host = "localhost", port =3306, user = sys.argv[1],passwd = sys.argv[2], db = sys.argv[3])
+    username = sys.argv[1]
+    pas = sys.argv[2]
+    dbn = sys.argv[3]
+    statex = sys.argv[4]
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=username, passwd=pas, db=dbn)
     cur = db.cursor()
-    cur.execute("SELECT cities.name FROM cities WHERE cities.state_id\
+    cur.execute("""SELECT cities.name FROM cities WHERE cities.state_id\
                  IN (SELECT id FROM states WHERE name=%s)\
-                 ORDER BY cities.id", [argv[4]])
-    row = cur.fetchall()
-    print(", ".join(i[0] for i in row))
+                 ORDER BY cities.id""", (statex, ))
+    rows = cur.fetchall()
+    print(", ".join(city[0] for city in rows))
     cur.close()
     db.close()
