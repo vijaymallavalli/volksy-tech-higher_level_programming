f84238b4eadf5f37e4ef9a3fcf7a7532667084fe
vijay mallavalli <vijaysaradhim@brightcomgroup.com>
2023-02-09 02:55:32 -0800

    d

--- SQL_more_queries/100-not_my_genres.sql
+++ SQL_more_queries/100-not_my_genres.sql
@@ -6,6 +6,6 @@ FROM tv_genres
 LEFT OUTER JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
 LEFT OUTER JOIN tv_shows
-ON tv_show_genres.show_id = tv_show.id WHERE tv_shows.title = 'Dexter'
+ON tv_show_genres.show_id = tv_shows.id WHERE tv_shows.title = 'Dexter'
 ORDER BY tv_genres.name)
 ORDER BY tv_genres.name;
