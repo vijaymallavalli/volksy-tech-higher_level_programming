1bb853cbdfb46ec2e008aa1c37b49801ba77350e
vijay mallavalli <vijaysaradhim@brightcomgroup.com>
2023-02-12 20:39:36 -0800

    d

---
+++ python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""list states"""
+
+from relationship_state import State
+from relationship_city import Base, City
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+import sys
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    DB_name = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(username, password, DB_name),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).order_by(State.id)
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
+        for cities in state.cities:
+            print("    {}: {}".format(cities.id, cities.name))
+    session.close()
