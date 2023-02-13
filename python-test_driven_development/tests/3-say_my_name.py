c476db0f25096e481a283d49cfbbcfadf538ab9f
vijay mallavalli <vijaysaradhim@brightcomgroup.com>
2023-02-12 23:15:27 -0800

    d

---
+++ python-test_driven_development/tests/3-say_my_name.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+module say_my_name
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    say_my_name
+    """
+    if type(first_name) != str:
+        raise TypeError('first_name must be a string')
+    if type(last_name) != str:
+        raise TypeError('last_name must be a string')
+    print("My name is " + first_name + " " + last_name)
