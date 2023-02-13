e56e2875ee2dc842ff31209d0635814cbf836207
vijay mallavalli <vijaysaradhim@brightcomgroup.com>
2023-02-13 01:19:29 -0800

    d

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+say_my_name
+"""
+
+
+def print_square(size):
+    """
+    print a square
+    """
+    if type(size) != int:
+        raise TypeError('size must be an integer')
+    if size < 0:
+        raise ValueError('size must be >= 0')
+    if size == 0:
+        raise TypeError('size must be an integer')
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print()
