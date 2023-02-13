21906d98ea1fb6c347d14efcd91a190a3bbe3e19
vijay mallavalli <vijaysaradhim@brightcomgroup.com>
2023-02-13 01:42:44 -0800

    4-print_square.py

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -13,7 +13,7 @@ def print_square(size):
     if size < 0:
         raise ValueError('size must be >= 0')
     if size == 0:
-        raise TypeError('size must be an integer')
+        return
     for i in range(size):
         for j in range(size):
             print("#", end="")
